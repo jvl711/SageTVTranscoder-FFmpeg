af78f909e77d37079e06dcb0294f334cc0ed972b
Joshua Lewis <jvl711@gmail.com>
2022-07-04 14:03:00 -0400

    Release to Github Testing

--- build.sh
+++ build.sh
@@ -514,7 +514,6 @@ if [ $1 = "build" ] || [ $1 = "buildall" ]; then
         --enable-nvenc \
         --enable-cuvid \
         --enable-cuda \
-        --enable-libmfx \
 		--disable-ffplay \
 		--disable-ffprobe \
 		--enable-gpl \
