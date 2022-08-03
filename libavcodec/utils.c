797c14900ca54d9eedfb96a5b859020a6a2ad7c9
Joshua Lewis <jvl711@workstation.core.joshandmonique.com>
2022-08-02 20:21:05 -0400

    Fixing build

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -512,6 +512,34 @@ size_t av_get_codec_tag_string(char *buf, size_t buf_size, unsigned int codec_ta
 
 /*
  * SAGETV CUSTOMIZATION
+ * This method was removed from the base FFmpeg repo
+ */
+static int64_t get_bit_rate(AVCodecContext *ctx)
+{
+    int64_t bit_rate;
+    int bits_per_sample;
+
+    switch (ctx->codec_type) {
+    case AVMEDIA_TYPE_VIDEO:
+    case AVMEDIA_TYPE_DATA:
+    case AVMEDIA_TYPE_SUBTITLE:
+    case AVMEDIA_TYPE_ATTACHMENT:
+        bit_rate = ctx->bit_rate;
+        break;
+    case AVMEDIA_TYPE_AUDIO:
+        bits_per_sample = av_get_bits_per_sample(ctx->codec_id);
+        bit_rate = bits_per_sample ? ctx->sample_rate * (int64_t)ctx->channels * bits_per_sample : ctx->bit_rate;
+        break;
+    default:
+        bit_rate = 0;
+        break;
+    }
+    return bit_rate;
+}
+
+
+/*
+ * SAGETV CUSTOMIZATION
  * Customized the whole method to match the other output format
  */
 void sagetv_avcodec_string(char *buf, int buf_size, AVCodecContext *enc, AVRational fps, int encode)
